9f10f91f943eb60b210c0f1e3256ba62cb2b78af
Carsten Dreesbach <carsten.dreesbach@opower.com>
2013-07-16 22:29:57 -0400

    Initial working version

--- yardoc.py
+++ yardoc.py
@@ -3,11 +3,23 @@ Sublime Yardoc
 by Revath S Kumar
 https://github.com/revathskumar/sublime-yardoc
 """
+import sublime
 import sublime_plugin
 import re
 
 
 class YardocCommand(sublime_plugin.TextCommand):
+
+    def load_config(self):
+        self.settings = {}
+        settings = sublime.load_settings('Sublime-Yardoc.sublime-settings')
+        for setting in ['trailing_spaces', 'initial_empty_line']:
+            if settings.get(setting) is None:
+                continue
+            self.settings[setting] = settings.get(setting)
+        self.trailing_spaces = ""
+        if settings.get('trailing_spaces'):
+            self.trailing_spaces = " "
 
     def counter(self):
         count = 0
@@ -15,45 +27,77 @@ class YardocCommand(sublime_plugin.TextCommand):
             count += 1
             yield(count)
 
-    def setTabIndex(self, doc):
+    def set_tab_index(self, doc):
         if doc:
             tabIndex = self.counter()
             for index, outputLine in enumerate(doc):
                 doc[index] = re.sub("(\\$\\{)\\d+(:[^}]+\\})", lambda m: "%s%d%s" % (m.group(1),  tabIndex.next() if hasattr(tabIndex, 'next') else next(tabIndex), m.group(2)), outputLine)
         return doc
 
-    def write(self, view, str):
+    def write(self, view, str, point):
         if None == str:
             str = self.line_ending()
-        view.run_command(
+        self.view.sel().clear()
+        self.view.sel().add(point)
+        print "New selection point: "
+        print self.view.sel()
+        print "write str: [" + str + "]"
+        # auto_indent = self.view.settings().get('auto_indent')
+        # indent_subsequent_lines = self.view.settings().get('indent_subsequent_lines')
+        # print "auto_indent: "
+        # print auto_indent
+        # self.view.settings().set('auto_indent', False)
+        # self.view.settings().set('indent_subsequent_lines', False)
+        # print "auto_indent: "
+        # print self.view.settings().get('auto_indent')
+        # print self.view.settings().get('indent_subsequent_lines')
+        self.view.run_command(
             'insert_snippet', {
                 'contents': str.decode('utf-8') if hasattr(str, 'decode') else bytes(str, 'utf-8').decode('utf-8')
             }
         )
+        # self.view.settings().set('auto_indent', auto_indent)
+        # self.view.settings().set('indent_subsequent_lines', indent_subsequent_lines)
+        # print self.view.settings().get('auto_indent')
+        # print self.view.settings().get('indent_subsequent_lines')
 
     def run(self, edit):
+        self.load_config()
         point = self.view.sel()[0].end()
+        # mark = [s for s in self.view.sel()]
+        # self.view.add_regions("mark", mark, "mark", "dot",
+        #                       sublime.HIDDEN | sublime.PERSISTENT)
+        print "Original point: " + str(point)
         scope = self.view.scope_name(point)
         if not re.search("source\\.ruby", scope):
             self.view.insert(edit, point, self.line_ending())
             return
-        line = self.read_line(point + 1)
-        if not self.check_doc(point):
+        line = self.read_line(point)
+        print "First read_line: " + line
+        if not self.check_doc(line):
             self.view.insert(edit, point, self.line_ending())
             return
         doc = self.compose_doc(line, edit)
-        self.write(self.view, doc)
+        print "Doc: [" + doc + "]"
+        point = self.view.line(point).begin()
+        print "New point before write: " + str(point)
+        self.view.insert(edit, point, self.line_ending())
+        self.write(self.view, doc, point)
+        print "edit: " + str(edit)
+        # self.view.erase(edit, self.view.get_regions("mark")[0])
 
-    def check_doc(self, point):
-        current_line = self.read_line(point)
-        params_match = re.search('#[ ]+@return|#[ ]+@param | # @author | # | # @', current_line)
+    def check_doc(self, line):
+        params_match = re.search('#\s+@return |#\s+@param |#\s+@author |# ?|#\s+@', line)
+        print "params_match: " + str(params_match)
         if not params_match:
+            print "check_doc return True"
             return True
+        print "check_doc return False"
         return False
 
     def get_author(self):
         author = "${1:[author]}"
-        return ["# ", "# @author " + author, "# "]
+        return ["#" + self.trailing_spaces, "# @author " + author, "#" + self.trailing_spaces]
 
     def line_ending(self):
         ending = "\n"
@@ -63,48 +107,62 @@ class YardocCommand(sublime_plugin.TextCommand):
 
     def format_lines(self, indent, lines):
         ending = self.line_ending()
-        lines = self.setTabIndex(lines)
-        return ending + (ending + indent).join(lines)
+        lines = self.set_tab_index(lines)
+        # indent = ""
+        return indent + (ending + indent).join(lines)
 
     def method_doc(self, params_match, current_line):
         params = [p.strip() for p in params_match.group(1).split(',') if len(p.strip()) > 0]
 
-        indent = re.search('(^ *)', current_line).group(0)
-        col = self.view.rowcol(self.view.sel()[0].end())[1]
+        indent = re.search('(^\s*)', current_line).group(0)
+        print "indent: [" + indent + "]"
+        # col = self.view.rowcol(self.view.sel()[0].end())[1]
+        # print "col: " + str(col)
 
-        if(col != 0):
-            indent = " " * (len(indent) - col)
+        # if(col != 0):
+        #     indent = " " * (len(indent) - col)
+        # print "new indent: [" + indent + "]"
 
-        method_name = re.search("def (?P<name>[a-zA-Z_]+|[a-zA-Z_]+[!|?])(?P<params>[(| ][a-zA-Z,]+|)", current_line).group("name")
-        lines = [indent + "# ", "# ${1:[%s description]}" % (method_name)]
+        # includes all operator methods as per http://stackoverflow.com/a/10542599/120818
+        method_name = re.search("def (?P<name>[a-zA-Z_][a-zA-Z_0-9]+[!?=]?|~|\+|\*\*|-|\*|/|%|<<|>>|&|\||\^|<=>|<|<=|=>|>|==|===|!=|=~|!~|!|\[\]=|\[\])", current_line).group("name")
+        lines = []
+        if(self.settings.get('initial_empty_line')):
+            lines.append("#" + self.trailing_spaces)
+        lines.append("# ${1:[%s description]}" % (method_name))
 
         for param in params:
             lines.append("# @param  %s [${1:type}] ${1:[description]}" % (param))
 
-        lines.append("# ")
+        lines.append("#" + self.trailing_spaces)
         lines.append("# @return [${1:type}] ${1:[description]}")
 
         return self.format_lines(indent, lines)
 
     def module_doc(self, current_line):
-        indent = re.search('(^ *)', current_line).group(0)
-        col = self.view.rowcol(self.view.sel()[0].end())[1]
+        indent = re.search('(^\s*)', current_line).group(0)
+        # col = self.view.rowcol(self.view.sel()[0].end())[1]
 
-        if(col != 0):
-            indent = " " * (len(indent) - col)
+        # if(col != 0):
+        #     indent = " " * (len(indent) - col)
 
-        lines = [indent + "# ", "# ${1:[ module description]}"]
+        lines = []
+        if(self.settings.get('initial_empty_line')):
+            lines.append("#" + self.trailing_spaces)
+        lines.append("# ${1:[ module description]}")
         lines.extend(self.get_author())
         return self.format_lines(indent, lines)
 
     def class_doc(self, params_match, current_line):
-        indent = re.search('(^ *)', current_line).group(0)
-        col = self.view.rowcol(self.view.sel()[0].end())[1]
+        indent = re.search('(^\s*)', current_line).group(0)
+        # col = self.view.rowcol(self.view.sel()[0].end())[1]
 
-        if(col != 0):
-            indent = " " * (len(indent) - col)
+        # if(col != 0):
+        #     indent = " " * (len(indent) - col)
 
-        lines = [indent + "# ", "# ${1:[ class description]}"]
+        lines = []
+        if(self.settings.get('initial_empty_line')):
+            lines.append("#" + self.trailing_spaces)
+        lines.append("# ${1:[ class description]}")
         lines.extend(self.get_author())
         return self.format_lines(indent, lines)
 
@@ -123,8 +181,8 @@ class YardocCommand(sublime_plugin.TextCommand):
         if (point >= self.view.size()):
             return
 
-        next_line = self.view.line(point)
-        return self.view.substr(next_line)
+        line = self.view.line(point)
+        return self.view.substr(line)
 
 
 class AddhashtagCommand(YardocCommand):
@@ -135,5 +193,6 @@ class AddhashtagCommand(YardocCommand):
         if not re.search("source\\.ruby", scope):
             self.view.insert(edit, point, ending)
             return
+        # line = ending + "#" + self.trailing_spaces
         line = ending + "# "
-        self.write(self.view, line)
+        self.write(self.view, line, point)
